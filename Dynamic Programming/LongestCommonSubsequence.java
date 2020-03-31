3d480618a9733c0f162fe467e09773473ca6245a
dipeshpatil <thedipeshpatil@gmail.com>
2020-03-31 22:36:58 +0530

    Update LongestCommonSubsequence.java

--- Dynamic Programming/LongestCommonSubsequence.java
+++ Dynamic Programming/LongestCommonSubsequence.java
@@ -2,14 +2,24 @@ package DynamicProgramming;
 
 public class LongestCommonSubsequence {
     public static void main(String[] args) {
-        System.out.println(lcs("XMASDFOP", "XMDOP"));
+        String[] soln = new String[]{
+                lcs("AGGTAB", "GXTXAYB"),
+                lcs("SDLPCMA", "SPLCMA")
+        };
+
+        for(String sol : soln)
+            System.out.println(sol);
+
+        //  GTAB 
+        //  SPCMA
     }
 
-    private static int lcs(String A, String B) {
+    private static String lcs(String A, String B) {
         char[] X = A.toCharArray();
         char[] Y = B.toCharArray();
         int m = X.length;
         int n = Y.length;
+
         int[][] L = new int[m + 1][n + 1];
 
         for (int i = 0; i <= m; i++) {
@@ -22,6 +32,26 @@ public class LongestCommonSubsequence {
                     L[i][j] = Math.max(L[i - 1][j], L[i][j - 1]);
             }
         }
-        return L[m][n];
+        int len = L[m][n]; // Length Of LCS
+        int temp = len;
+        char[] lcs = new char[len + 1];
+        StringBuilder res = new StringBuilder();
+
+        int i = m, j = n;
+        while (i > 0 && j > 0) {
+            if (X[i - 1] == Y[j - 1]) {
+                lcs[len - 1] = X[i - 1];
+                i--;
+                j--;
+                len--;
+            } else if (L[i - 1][j] > L[i][j - 1])
+                i--;
+            else
+                j--;
+        }
+        for (int k = 0; k <= temp; k++) {
+            res.append(lcs[k]);
+        }
+        return res.toString();
     }
 }
