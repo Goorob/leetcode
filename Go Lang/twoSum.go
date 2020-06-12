f570979216dafd5bef630c97c3661fbf38796ec5
dipeshpatil <thedipeshpatil@gmail.com>
2020-06-12 10:25:29 +0530

    Update twoSum.go

--- Go Lang/twoSum.go
+++ Go Lang/twoSum.go
@@ -5,25 +5,24 @@ import (
 	"sort"
 )
 
-	func main() {
-		twoSum([]int{1, 2, 3, 4}, 6)
-	}
+func main() {
+	twoSum([]int{1, 2, 3, 4}, 6)
+}
 
-	func twoSum(arr []int, target int) {
-		sort.Ints(arr)
-		left := 0
-		right := len(arr) - 1
+func twoSum(arr []int, target int) {
+	sort.Ints(arr)
+	left := 0
+	right := len(arr) - 1
 
-		for left < right {
-			currentSum := arr[left] + arr[right]
-			if currentSum == target {
-				fmt.Printf("[%d , %d]", arr[left], arr[right])
-				return
-			} else if currentSum < target {
-				left++
-			} else {
-				right--
-			}
+	for left < right {
+		currentSum := arr[left] + arr[right]
+		if currentSum == target {
+			fmt.Printf("[%d , %d]", arr[left], arr[right])
+			return
+		} else if currentSum < target {
+			left++
+		} else {
+			right--
 		}
 	}
-
+}
