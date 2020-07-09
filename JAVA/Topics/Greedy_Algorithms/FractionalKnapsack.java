5bb7b341d561982c431495df4bc200efbb55f6a0
dipeshpatil <thedipeshpatil@gmail.com>
2020-07-09 21:16:20 +0530

    Update FractionalKnapsack.java

--- JAVA/Topics/Greedy_Algorithms/FractionalKnapsack.java
+++ JAVA/Topics/Greedy_Algorithms/FractionalKnapsack.java
@@ -1,118 +1,111 @@
+package DynamicProgramming;
+
 import java.util.Arrays;
 
 /**
+ * Stability : stable
  * Fractional Knapsack - Greedy Approach
- * Overall Complexity - O(N*N)
  * Algorithm Complexity - O(N)
  */
 
-public class FractionalKnapsack {
-    public static void main(String[] args) {
-        FKnap(new int[]{18, 5, 9, 10, 12, 17}, new int[]{7, 2, 3, 5, 3, 2}, 13);
+public class FractionalKnapsack{
+    public static class Item{
+        int profit, weight;
+        double pbw;
 
-        /**
-           Item		Profit		Weight		P/W
-           1		17		2		8.5
-           2		12		3		4.0
-           3		9		3		3.0
-           4		18		7		2.57
-           5		5		2		2.5
-           6		10		5		2.0
-           Total Profit: 50.86
-           Total Weight: 13.0
-           Fraction of Items Taken: [1.0, 1.0, 1.0, 0.71, 0.0, 0.0]
-         */
+        Item(int profit,int weight){
+            this.profit = profit;
+            this.weight = weight;
+            this.pbw = roundOff((profit * 1.0) / weight);
+        }
     }
 
-    private static void FKnap(int[] profits, int[] weights, int sackCapacity) {
-        if (profits.length != weights.length) {
-            System.out.println("profits[] and weights[] must be of same size");
-            return;
-        }
-
-        int N = profits.length; // No of items.
-        double CW = 0, CP = 0;
+    private static void fKnapsack(Item[] items,int sackCapacity){
+        int N = items.length;
         double[] X = new double[N];
-        double[] pbw = new double[N];
+        double cW = 0, cP = 0;
 
-        for (int i = 0; i < N; i++)
-            pbw[i] = roundOff(((profits[i] * 1.0) / weights[i]));
-
-        int[][] sorted = customSort(profits, weights, sort(pbw));
-        // Sort both the arrays according to profits by weights
-
-        profits = sorted[0];
-        // Get the new profits sorted according to profits by weights
-
-        weights = sorted[1];
-        // Get the new weights sorted according to profits by weights
-
-        System.out.println("Item\tProfit\tWeight\tP/W");
+        System.out.println("Item\t\tProfit\t\tWeight\t\tP/W");
 
         for (int i = 0; i < N; i++) {
-            if (CW + weights[i] < sackCapacity) {
+            Item currentItem = items[i];
+            if (cW + currentItem.weight < sackCapacity) {
                 X[i] = 1;
-                CP += profits[i];
-                CW += weights[i];
+                cP += currentItem.profit;
+                cW += currentItem.weight;
             } else {
-                X[i] = ((sackCapacity - CW) * 1.0) / weights[i];
-                CP += profits[i] * X[i];
+                X[i] = ((sackCapacity - cW) * 1.0) / currentItem.weight;
                 X[i] = roundOff(X[i]);
-                CW = sackCapacity;
+                cP += currentItem.profit * X[i];
+                cW = sackCapacity;
             }
-            System.out.println(String.format("%s\t\t%s\t\t%s\t\t%s", i + 1, profits[i], weights[i], pbw[i]));
+            System.out.println(
+                    String.format(
+                            "%d\t\t\t%d\t\t\t%d\t\t\t%.2f",
+                            i + 1,
+                            currentItem.profit,
+                            currentItem.weight,
+                            currentItem.pbw
+                    ));
         }
 
-        System.out.println("Total Profit: " + roundOff(CP));
-        System.out.println("Total Weight: " + roundOff(CW));
+        System.out.println("\nTotal Profit: " + cP);
+        System.out.println("Total Weight: " + cW);
         System.out.println("Fraction of Items Taken: " + Arrays.toString(X));
     }
 
-    // Rounding off to 2 decimal places
-    private static double roundOff(double x) {
-        return Math.round(x * 100.0) / 100.0;
-    }
-
-    // Utility function to sort according to Profits by Weights;
-    private static int[][] customSort(int[] profits, int[] weights, double[] pbw) {
-        int N = profits.length;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (pbw[i] == roundOff((profits[j] * 1.0) / weights[j])) {
-                    swap(i, j, profits);
-                    swap(i, j, weights);
-                }
-            }
-        }
-        return new int[][]{profits, weights};
-    }
-
-    // A utility bubblesort function to sort an array in descending order
-    private static double[] sort(double[] arr) {
+    private static void sortItemsByPbW(Item[] items){
         boolean isSorted = false;
         int counter = 0;
         while (!isSorted) {
             isSorted = true;
-            for (int i = 0; i < arr.length - 1 - counter; i++) {
-                if (arr[i] < arr[i + 1]) {
-                    swap(i, i + 1, arr);
+            for (int i = 0; i < items.length - 1 - counter; i++) {
+                if (items[i].pbw < items[i + 1].pbw) {
+                    swap(i,i + 1,items);
                     isSorted = false;
                 }
             }
             counter++;
         }
-        return arr;
     }
 
-    private static void swap(int i, int j, int[] arr) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    private static double roundOff(double x){
+        return Math.round(x * 100.0) / 100.0;
     }
 
-    private static void swap(int i, int j, double[] arr) {
-        double temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    private static void swap(int i,int j,Item[] items){
+        Item temp = items[i];
+        items[i] = items[j];
+        items[j] = temp;
+    }
+
+    public static void main(String[] args){
+        Item[] items = new Item[]{
+                new Item(18,7),
+                new Item(5,2),
+                new Item(9,3),
+                new Item(10,5),
+                new Item(12,3),
+                new Item(17,2),
+        };
+        int sackCapacity = 13;
+
+        sortItemsByPbW(items);
+
+        fKnapsack(items,sackCapacity);
+
+        /*
+            Item		Profit		Weight		P/W
+            1			17			2			8.50
+            2			12			3			4.00
+            3			9			3			3.00
+            4			18			7			2.57
+            5			5			2			2.50
+            6			10			5			2.00
+
+            Total Profit: 50.78
+            Total Weight: 13.0
+            Fraction of Items Taken: [1.0, 1.0, 1.0, 0.71, 0.0, 0.0]
+        */
     }
 }
